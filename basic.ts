e5252631dd299ae3995791a1b0033345dfce2e67
Gnlow <gnlow@protonmail.com>
2024-04-20 16:56:41 +0900

    a

---
+++ basic.ts
@@ -0,0 +1,21 @@
+import {
+    load,
+    Prolog,
+} from "https://esm.sh/trealla@0.17.41"
+
+await load()
+
+const pl = new Prolog()
+
+await pl.consultText(`
+    like(a, b).
+    like(a, d).
+`)
+
+const query = pl.query(`
+    like(a, X).
+`)
+
+for await (const answer of query) {
+    console.log(answer)
+}
