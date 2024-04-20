08dd23e54dbb6e2164994e9f89d9453fbce60a3c
Gnlow <gnlow@protonmail.com>
2024-04-20 16:45:15 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,18 +1,32 @@
 import {
     load,
     Prolog,
+    Predicate,
+    Compound,
 } from "https://esm.sh/trealla@0.17.41"
 
 await load()
 
 const pl = new Prolog()
 
+const succ = new Predicate<Compound<
+    "succ", [number, number]
+>>(
+    "succ", 2,
+    async function*(_pl, _subq, goal) {
+        const [a, b] = goal.args
+        goal.args[1] = a + 1
+        yield goal
+    }
+    
+)
+await pl.register(succ)
+
 await pl.consultText(`
-    like(a, b).
 `)
 
 const query = pl.query(`
-    like(a, X).
+    succ(1, Y).
 `)
 
 for await (const answer of query) {
